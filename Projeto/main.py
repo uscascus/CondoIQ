91d4b4895fe4f517b581f0581ef5580fe2b94fa0
Diogo Borges de Moura <111078132+DigogSXD@users.noreply.github.com>
2025-08-22 14:47:56 -0300

    Delete Projeto directory

--- Projeto/main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from flask import Blueprint, render_template
-
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def home():
-    # Isso renderiza o arquivo index.html que está na pasta 'templates'
-    return render_template('index.html')
